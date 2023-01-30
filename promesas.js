c3dfef588a93b61118689ee193e656e0258a326a
Jose Luis Gallardo <gallardohjl2@gmail.com>
2023-01-30 13:16:26 -0600

    commit promesas termidas

--- promesas.js
+++ promesas.js
@@ -20,3 +20,89 @@ const usuarioAutenticado = new Promise(
 usuarioAutenticado
     .then((resultado) => console.log(resultado))
     .catch((error) => console.log(error))
+
+    /*function cuadradoPromesa(value) {
+
+    if (typeof value !== "number") {
+        return Promise.reject(`Error el valor ${value} no es un número`);
+    }
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve({
+                value,
+                result: Math.pow(value, 2)
+            });
+        }, 3000);
+    })
+
+}*/
+
+const cuadradoPromesa = value =>{
+
+    if (typeof value !== "number") {
+        return Promise.reject(`Error el valor ${value} no es un número`);
+    }
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve({
+                value,
+                result: Math.pow(value, 2)
+            });
+        }, 3000);
+    })
+
+}
+
+
+
+
+cuadradoPromesa(2)
+    .then((respuesta) => {
+        console.log('Inicia Promesa');
+        console.log(
+            `
+        Promesa: valor: ${respuesta.value} 
+        cuadrado: ${respuesta.result}
+        
+        `
+        );
+
+        return cuadradoPromesa(3)
+    })
+    .then((respuesta) => {
+        console.log('Inicia Promesa');
+        console.log(
+            `
+           Promesa: valor: ${respuesta.value} 
+           cuadrado: ${respuesta.result}
+           
+           `
+        );
+
+        return cuadradoPromesa("x")
+    })
+    .then((respuesta) => {
+        console.log('Inicia Promesa');
+        console.log(
+            `
+           Promesa: valor: ${respuesta.value} 
+           cuadrado: ${respuesta.result}
+           
+           `
+        );
+
+        return cuadradoPromesa(5)
+    })
+    .then((respuesta) => {
+        console.log('Inicia Promesa');
+        console.log(
+            `
+           Promesa: valor: ${respuesta.value} 
+           cuadrado: ${respuesta.result}
+           
+           `
+        );
+    })
+    .catch(err => console.log(err))
